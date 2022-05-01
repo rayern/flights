b7e81824fe506d927ec18d33356aeaefe9db377a
saeeshtendulkar@gmail.com <saeesh@softmonks.com>
2022-05-01 19:37:15 +0530

    dotenv added

--- autoload.php
+++ autoload.php
@@ -1,4 +1,8 @@
 <?php
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 function loadLibs($class){
     require_once __DIR__.'/'.str_replace('\\','/',$class).".php";
