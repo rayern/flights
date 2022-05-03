7fc06cd7a0584534ce7c3138b062d3d9252260f5
Saeesh <saeeshtendulkar@gmail.com>
2022-05-03 14:30:09 +0530

    Update Database.php

--- database/Database.php
+++ database/Database.php
@@ -3,9 +3,19 @@
 namespace Database;
 
 class Database{
-    private $connection, $queryParams, $query, $enableLog = false;
-    public function __construct()
-    {
+    private $connection, $queryParams, $params, $query;
+    public function __construct(){
+        $default_options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+        $dsn = sprintf('mysql:dbname=%s;host=%s', $_ENV['DB_NAME'], $_ENV['DB_HOST']);
+        try {
+            $this->connection = new \PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
     }
     public function table($table){
         $this->queryParams['table'] = $table;
@@ -14,7 +24,8 @@ class Database{
         $this->queryParams['select'] = $select;
     }
     public function where($param, $value, $op = "="){
-        $this->queryParams['where'] = $param.$op.$value;
+        $this->queryParams['whereQuery'][] = $param.$op.':'.$param;
+        $this->params[$param] = $value;
     }
     public function order($order){
         $this->queryParams['order'] = $order;
@@ -22,50 +33,39 @@ class Database{
     public function limit($limit){
         $this->queryParams['limit'] = $limit;
     }
+    public function applyWhere(){
+    }
     public function get(){
         $this->query = "SELECT ".$this->queryParams['select'].
-        " FROM ".$this->queryParams['table'].
-        " WHERE ".$this->queryParams['where'].
-        " ORDER BY ".$this->queryParams['where'].
-        " LIMIT ".$this->queryParams['limit'];
-        if($this->enableLog){
-            $dbLog = fopen(__DIR__ . "/../storage/log/db.log", "a");
-            $txt = date('Y-m-d H:i:s')." | ".$this->query."\n";
-            fwrite($dbLog, $txt);
-            fclose($myfile);
+        " FROM ".$this->queryParams['table'];
+        if(is_array(this->queryParams['whereQuery'])){
+            $this->query .= " WHERE ";
+            foreach(this->queryParams['whereQuery'] as $whereQuery){
+                $this->query .= $whereQuery." and ";
+            }
+            $this->query = rtrim($this->query," and ");
         }
-    }
-    public function enableQueryLog(){
-        $this->enableLog = true;
+        $this->query .= "ORDER BY ".$this->queryParams['where'].
+        " LIMIT ".$this->queryParams['limit'];
+        return $this->run($this->query, $this->params)->fetchAll();
     }
 
     public function insert($params){
-        $this->query = "INSERT INTO ".$this->queryParams['table'].'('.array_keys($params).') VALUES ('.array_values($params).')';
-
+        $this->query = "INSERT INTO ".$this->queryParams['table'].' ('.implode(",",array_keys($params)).') VALUES ('.implode(",",preg_filter('/^/', ':',array_keys($params))).')';
     }
 
     public function update($params){
-
+        $this->query = "UPDATE ".$this->queryParams['table'].' SET '.implode(",", array_keys($params)).' WHERE ('.preg_filter('/^/', ':',array_keys($params)).')';
     }
-
-    public function runSQL(){
-        try{
-            $dsn = sprintf('mysql:dbname=%s;host=%s', $_ENV['DB_NAME'], $_ENV['DB_HOST']);
-            $options = [
-                \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
-            $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $options);
-
-            $statement = $pdo->prepare($this->query);
-
-            $statement->execute(array(
-                'name' => $name,
-            ));
-        }catch(\Exception $e){  
-            Echo "Connection failed" . $e->getMessage();  
-        }  
+    
+    public function run($sql, $args = NULL)
+    {
+        if (!$args)
+        {
+            return $this->connection->query($sql);
+        }
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
     }
-
 }
